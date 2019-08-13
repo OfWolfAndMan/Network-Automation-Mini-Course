0baacd6cb37d0af70d33337244947da3463278f4
OfWolfAndMan <therootbridge@hotmail.com>
2019-08-12 22:14:29 -0400

    Added initial Jinja2 config

--- render_templates.py
+++ render_templates.py
@@ -1,17 +1,27 @@
 from jinja2 import Environment, FileSystemLoader
+import os
 
-ENV = Environment(loader=FileSystemLoader('./configTemplates/IOS'))
-try:
-    template = ENV.get_template("interface.j2")
-    print("Template was found!")
-except FileNotFoundError:
-    print("The file was not found!")
 
-class L2Interface(object):
-    def __init__(self, description, intname, vlan=10, voice_vlan=20):
-        self.host_description = host_description
-        self.trunk_description = trunk_description
-        self.intname = intname
-        self.vlan = vlan
-        self.voice_vlan = voice_vlan
+def render_template(Hostname, Template, Model, NOS="IOS"):
+    DirectoryToAdd = f"./renderedTemplates/{NOS}/{Model}"
+    ENV = Environment(loader=FileSystemLoader(f'./configTemplates/{NOS}'))
+    try:
+        template = ENV.get_template(f"{Template}.j2")
+        print("Template was found!")
+    except FileNotFoundError:
+        print("The file was not found!")
 
+    class L2Interface(object):
+        def __init__(self, intname, trunk_description="<===Trunk Port===>", host_description="<===User Port===>", vlan=10, voice_vlan=20):
+            self.trunk_description = trunk_description
+            self.host_description = host_description
+            self.intname = intname
+            self.vlan = vlan
+            self.voice_vlan = voice_vlan
+
+
+    if not os.path.exists(DirectoryToAdd):
+        os.mkdir(DirectoryToAdd)
+
+    with open(f"{DirectoryToAdd}/{Hostname}.txt", 'w') as renderedTemplate:
+        renderedTemplate.write(template.render(interface=interface_object))
