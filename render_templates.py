948c1659cbe20a04b97913567fd0bf38f7c5845c
OfWolfAndMan <therootbridge@hotmail.com>
2019-08-23 12:56:19 -0400

    Updated templates; Made the template renderer more modular

--- render_templates.py
+++ render_templates.py
@@ -3,18 +3,23 @@ import os
 from configTemplates.objects import L2Interface
 
 
-def render_template(Hostname, Template, Model, NOS="IOS"):
+def render_template(Hostname, Template, Model, NOS="IOS", vendor="Cisco", **kwargs):
     DirectoryToAdd = f"./renderedTemplates/{NOS}/{Model}"
     ENV = Environment(loader=FileSystemLoader(f"./configTemplates/{NOS}"))
+    params = {"hostname": Hostname, "vendor": vendor, "model": Model}
     try:
         template = ENV.get_template(f"{Template}.j2")
     except FileNotFoundError:
-        print("The file was not found!")
+        print(f"The template {Template} is not valid!")
 
-    intf_object = L2Interface("GigabitEthernet0/", vlan=15)
+    if Template == 'interface':
+        intf_object = L2Interface(kwargs['int_prefix'], kwargs['vlan'])
+        params["interface"] = intf_object
+    elif Template == 'compliance':
+        pass
 
     if not os.path.exists(DirectoryToAdd):
         os.mkdir(DirectoryToAdd)
 
     with open(f"{DirectoryToAdd}/{Hostname}.txt", "w") as renderedTemplate:
-        renderedTemplate.write(template.render(interface=intf_object))
+        renderedTemplate.write(template.render(**params))
--- test-render.py
+++ test-render.py
@@ -1,9 +1,9 @@
-# from render_templates import render_template
-# from resources import yaml_function
-#
-# devices = yaml_function("APIs/lab_devices.yml", "load")
-#
-# for device in devices:
-#     render_template(device.get("deviceName"), "interface", device.get("Model"))
+from render_templates import render_template
+from resources import yaml_function
+
+devices = yaml_function("APIs/lab_devices.yml", "load")
+
+for device in devices:
+    render_template(device.get("deviceName"), "interface", device.get("Model"), int_prefix="GigabitEthernet0/",  vlan=15)
 
 # breakpoint() - builtin function to replace pdb
